ec89ba294f6742c207acc1740485ef70f12c7995
sandeep pucha <sandeepsai1509@gmail.com>
2023-05-04 05:49:57 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import MultilingualGreetings from './Components/MultilingualGreetings'
+
+import './App.css'
+
+// Replace your code here
+const App = () => <MultilingualGreetings />
+
+export default App
